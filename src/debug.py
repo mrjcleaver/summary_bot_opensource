b7ae007ee6ada8a51f8627fa656308e5c2ad0b72
mrjcleaver2 <mrjcleaver2@gmail.com>
2025-03-25 15:20:35 -0400

    Refactor debug.py to use logging for debug messages and improve clarity

--- src/debug.py
+++ src/debug.py
@@ -1,10 +1,14 @@
 # This is imported by src/main.py only for debugging purposes. It is not used in production.
 import debugpy # noqa
 import os
+import logging
 
 debugpy.listen(("0.0.0.0", 5678))
-print("🪛 debugpy listening on port 5678...")
+logging.info("🪛 debugpy listening on port 5678...")
+debugpy.wait_for_client()  # noqa
+print("Breakpoint should hit here")  # ⛔ Set breakpoint here
+
 
 if os.getenv("DEBUGPY_WAIT") == "true":
-    print("🪛 debugpy enabled, now waiting_for_client")
+    logging.info("🪛 debugpy enabled, now waiting_for_client")
     debugpy.wait_for_client()  # noqa
